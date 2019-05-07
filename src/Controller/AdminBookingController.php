077bc4e10616c398c8c3114a652df5f7b1a5dab0
Nea18 <nanou718@hotmail.fr>
2019-05-07 09:32:50 +0200

    Mise en place de l'administration du site

---
+++ src/Controller/AdminBookingController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Booking;
+use App\Form\AdminBookingType;
+use App\Repository\BookingRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminBookingController extends AbstractController
+{
+    /**
+     * Permet d'afficher une réservation
+     * @Route("/admin/bookings", name="admin_booking_index")
+     */
+    public function index(BookingRepository $repo)
+    {
+        return $this->render('admin/booking/index.html.twig', [
+            'bookings' => $repo->findAll()
+        ]);
+    }
+
+    /**
+     * Permet d'éditer une réservation
+     * @Route("/admin/bookings/{id}/edit", name="admin_booking_edit")
+     * @return Response
+     */
+    public function edit(Booking $booking, Request $request, ObjectManager $manager)
+    {
+        $form = $this->createForm(AdminBookingType::class, $booking);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $booking->setAmount(0);
+            
+            $manager->persist($booking);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "La réservation n°<strong>{$booking->getId()}</strong> a bien été modifiée"
+            );
+
+            return $this->redirectToRoute('admin_booking_index');
+        }
+
+        return $this->render('admin/booking/edit.html.twig', [
+            'booking' => $booking,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet de supprimer une réservation
+     * @Route("/admin/bookings/{id}/delete", name="admin_booking_delete")
+     *
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function delete(Booking $booking, ObjectManager $manager)
+    {
+        $manager->remove($booking);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "La réservation a bien été supprimée."
+            );
+        
+        return $this->redirectToRoute('admin_booking_index');
+    }
+}
