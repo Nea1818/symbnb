28c3a33e763de40298dbc3d2fad5a07371a243c0
Nea18 <nanou718@hotmail.fr>
2019-08-23 18:27:00 +0200

    Update packages et fix HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HomeController extends AbstractController
+class HomeController extends Controller
 {
     /**
     * @Route("/", name="homepage")
